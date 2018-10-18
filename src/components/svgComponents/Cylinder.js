5d317e64f6c2c18419138d7d9841cf5e585561f6
Alexander Berezovsky <fantik217@gmail.com>
2018-10-18 10:09:47 +0700

    added

---
+++ src/components/svgComponents/Cylinder.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SvgCylinder = props => (
+  <svg viewBox="0 0 16 16" fill="none" width="1em" height="1em" {...props}>
+    <path
+      d="M8 2c3.9 0 6 1.5 6 2s-2.1 2-6 2-6-1.5-6-2 2.1-2 6-2zm0-2C3.6 0 0 1.8 0 4s3.6 4 8 4 8-1.8 8-4-3.6-4-8-4z"
+      fill="#434343"
+    />
+    <path
+      d="M8 12c-4 0-8-1.4-8-4 0-.6.4-1 1-1s1 .4 1 1c0 .7 2.3 2 6 2s6-1.3 6-2c0-.6.4-1 1-1s1 .4 1 1c0 2.6-4 4-8 4z"
+      fill="#434343"
+    />
+    <path
+      d="M8 16c-4 0-8-1.4-8-4 0-.6.4-1 1-1s1 .4 1 1c0 .7 2.3 2 6 2s6-1.3 6-2c0-.6.4-1 1-1s1 .4 1 1c0 2.6-4 4-8 4z"
+      fill="#434343"
+    />
+  </svg>
+);
+
+export default SvgCylinder;
