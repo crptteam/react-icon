f9cea6a085300a8bdd61510f16c949536938bf48
Shadwar <vladoivan@mail.ru>
2018-10-30 16:28:01 +0300

    feat/duplicate_select-all

---
+++ src/components/svgComponents/SelectAll.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SvgSelectAll = props => (
+  <svg viewBox="0 0 16 16" fill="#434343" width="1em" height="1em" {...props}>
+    <path d="M11.2 3H1.8C.8 3 0 3.8 0 4.8v9.4c0 1 .8 1.8 1.8 1.8h9.4c1 0 1.8-.8 1.8-1.8V4.8c0-1-.8-1.8-1.8-1.8zM11 14H2V5h9v9z" />
+    <path d="M14.2 0H5.8c-.6 0-1 .4-1 1s.4 1 1 1H14v8.2c0 .6.4 1 1 1s1-.4 1-1V1.8c0-1-.8-1.8-1.8-1.8zM6 12.4l3.7-3.7c.4-.4.4-1 0-1.4-.4-.4-1-.4-1.4 0L6 9.6l-.3-.3c-.4-.4-1-.4-1.4 0-.4.4-.4 1 0 1.4L6 12.4z" />
+  </svg>
+);
+
+export default SvgSelectAll;
