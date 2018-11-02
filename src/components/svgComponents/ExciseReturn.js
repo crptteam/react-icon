fe07b3cca4391b83d136455c1179edacb6ff9ca5
Alexander Berezovsky <fantik217@gmail.com>
2018-11-02 09:40:52 +0700

    added excise-return

---
+++ src/components/svgComponents/ExciseReturn.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SvgExciseReturn = props => (
+  <svg viewBox="0 0 16 16" fill="none" width="1em" height="1em" {...props}>
+    <path
+      d="M8 8c4.4 0 8-1.8 8-4s-3.6-4-8-4-8 1.8-8 4 3.6 4 8 4zm0-6c3.9 0 6 1.5 6 2s-2.1 2-6 2-6-1.5-6-2 2.1-2 6-2zM15 12H3.4l.3-.3c.4-.4.4-1 0-1.4-.4-.4-1-.4-1.4 0L.5 12.1c-.5.5-.5 1.3 0 1.8l1.8 1.8c.2.2.4.3.7.3.3 0 .5-.1.7-.3.4-.4.4-1 0-1.4l-.3-.3H15c.6 0 1-.4 1-1s-.4-1-1-1z"
+      fill="#434343"
+    />
+  </svg>
+);
+
+export default SvgExciseReturn;
