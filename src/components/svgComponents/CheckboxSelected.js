7a442bba65f966157f5b8b1e1bd3256bccaaa097
Ivan <vladoivan@mail.ru>
2018-11-06 13:35:00 +0300

    feature/checkbox_requests

---
+++ src/components/svgComponents/CheckboxSelected.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SvgCheckboxSelected = props => (
+  <svg width="1em" height="1em" viewBox="0 0 16 16" fill="#55B5E7" {...props}>
+    <path d="M5.7 6.255c-.4-.4-1-.4-1.4 0-.4.4-.4 1 0 1.4l3.7 3.8 7.7-8.8c.4-.4.3-1-.1-1.4-.4-.4-1-.3-1.4.1l-.3.4c-.5-.5-1.2-.7-2-.7H4.1c-1.7-.1-3.1 1.3-3.1 3v7.9c0 1.6 1.4 3 3.1 3H12c1.7 0 3.1-1.4 3.1-3.1v-6.2l-2 2.3v4c0 .6-.5 1.1-1.1 1.1H4.1c-.6-.1-1.1-.6-1.1-1.2v-7.8c0-.6.5-1.1 1.1-1.1H12c.3 0 .5.1.7.2L8 8.455l-2.3-2.2z" />
+  </svg>
+);
+
+export default SvgCheckboxSelected;
