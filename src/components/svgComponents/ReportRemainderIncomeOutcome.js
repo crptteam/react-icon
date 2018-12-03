a44ac3947b52c7f67a6303e40740609ce1c588b8
Alexander Berezovsky <fantik217@gmail.com>
2018-12-03 19:18:11 +0700

    FEAT: report icons

---
+++ src/components/svgComponents/ReportRemainderIncomeOutcome.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SvgReportRemainderIncomeOutcome = props => (
+  <svg width="1em" height="1em" viewBox="0 0 64 64" fill="none" {...props}>
+    <g
+      clipPath="url(#report-remainder-income-outcome_svg__clip0)"
+      fill="#434244"
+    >
+      <path d="M33.59 32.59L21 45.17V41c0-1.1-.9-2-2-2s-2 .9-2 2v11h11c1.1 0 2-.9 2-2s-.9-2-2-2h-4.17l12.59-12.59c.78-.78.78-2.05 0-2.83-.79-.78-2.05-.78-2.83.01zM22 32c0 1.1.9 2 2 2s2-.9 2-2V21H15c-1.1 0-2 .9-2 2s.9 2 2 2h4.17L7.59 36.59c-.78.78-.78 2.05 0 2.83.39.38.9.58 1.41.58.51 0 1.02-.2 1.41-.59L22 27.83V32z" />
+      <path d="M47.56 0H13.44C10.44 0 8 2.44 8 5.44V8H5.44C2.44 8 0 10.44 0 13.44v45.13c0 3 2.44 5.44 5.44 5.44h34.13c3 0 5.44-2.44 5.44-5.44V56h2.56c3 0 5.44-2.44 5.44-5.44V5.44C53 2.44 50.56 0 47.56 0zM41 58.56c0 .79-.65 1.44-1.44 1.44H5.44C4.65 60 4 59.36 4 58.56V13.44c0-.79.65-1.44 1.44-1.44h34.13c.79 0 1.44.65 1.44 1.44v45.12H41zm8-8c0 .79-.65 1.44-1.44 1.44H45V13.44c0-3-2.44-5.44-5.44-5.44H12V5.44c0-.79.65-1.44 1.44-1.44h34.13c.78 0 1.43.65 1.43 1.44v45.12z" />
+    </g>
+    <defs>
+      <clipPath id="report-remainder-income-outcome_svg__clip0">
+        <path fill="#fff" d="M0 0h53v64H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default SvgReportRemainderIncomeOutcome;
