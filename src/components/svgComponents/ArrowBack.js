0bc138d2fbc25202cdb0fdb10566ef2f99f75dc8
a.shirshov <as.shirshov@icloud.com>
2019-06-13 15:50:08 +0300

    ELK-4 New arrow back icon. 1.1.37 version

---
+++ src/components/svgComponents/ArrowBack.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ArrowBack = props => (
+    <svg fill="none" height={10} width={14} {...props}>
+        <g clipRule="evenodd" fillRule="evenodd">
+            <path d="M5.707.293a1 1 0 0 1 0 1.414L2.414 5l3.293 3.293a1 1 0 0 1-1.414 1.414l-4-4a1 1 0 0 1 0-1.414l4-4a1 1 0 0 1 1.414 0z" />
+            <path d="M2 5a1 1 0 0 1 1-1h10a1 1 0 1 1 0 2H3a1 1 0 0 1-1-1z" />
+        </g>
+    </svg>
+);
+
+export default ArrowBack
